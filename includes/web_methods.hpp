8ca0400681b8d691d6e5c978b5305978f4886577
hamzahassnaain <hamzahassanain067@gamil.com>
2025-08-16 17:11:16 +0300

    intal commit

---
+++ includes/web_methods.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+namespace hamza_web::methods
+{
+    extern const std::string GET;
+    extern const std::string POST;
+    extern const std::string PUT;
+    extern const std::string DELETE;
+    extern const std::string PATCH;
+    extern const std::string HEAD;
+    extern const std::string OPTIONS;
+}
